cf57ac14c6e2523d2c3e54098720935157e1755b
volca <volca@tom.com>
2017-07-14 15:32:30 +0800

    parse ble advertisement data

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
 const http = require('http');
 var parser = require('ble-ad-parser');
 
+const OFFSET_LEN        = 2;
+const OFFSET_ADV_TYPE   = 4;
+const OFFSET_MAC        = 6;
+const OFFSET_RSSI       = 18;
+const OFFSET_ADV_DATA   = 20;
+
+function isIbeacon(packet) {
+    console.log(packet);
+}
+
 const server = http.createServer((req, res) => {
-  const chunks = [];
-  let size = 0;
+    const chunks = [];
+    let size = 0;
 
-  req.on('data', (chunk) => {
-    chunks.push(chunk);
-    size += chunk.length;
-  });
+    req.on('data', (chunk) => {
+        chunks.push(chunk);
+        size += chunk.length;
+    });
 
-  req.on('end', () => {
-    let data = null;
-    switch (chunks.length) {
-      case 0: data = chunks[0];
-        break;
-      case 1: data = chunks[0];
-        break;
-      default:
-        data = new Buffer(size);
-        for (let i = 0, pos = 0, len = chunks.length; i < len; i++) {
-          const chunk = chunks[i];
-          chunk.copy(data, pos);
-          pos += chunk.length;
+    req.on('end', () => {
+        let data = null;
+        switch (chunks.length) {
+            case 0: data = chunks[0];
+                break;
+            case 1: data = chunks[0];
+                break;
+            default:
+                data = new Buffer(size);
+                for (let i = 0, pos = 0, len = chunks.length; i < len; i++) {
+                    const chunk = chunks[i];
+                    chunk.copy(data, pos);
+                    pos += chunk.length;
+                }
+                break;
         }
-        break;
-    }
-    data = data.toString('hex');
-    const head = decodeURIComponent(data.match(/^7b\S+0d0a0d0a/)[0].replace(/(\w{2})/g, '%$1'));
-    data = data.replace(head, '');
-    data = data.replace(/(0d0a)/gm, '$1\n');
-    const bles = data.match(/fe\w+0d0a/gm);
-    console.log(head)   // 元数据
-    console.log(bles)   // 扫描到的蓝牙设备数据
-    res.end('post')
-  });
+        data = data.toString('hex');
+        const head = decodeURIComponent(data.match(/^7b\S+0d0a0d0a/)[0].replace(/(\w{2})/g, '%$1'));
+        data = data.replace(head, '');
+        data = data.replace(/(0d0a)/gm, '$1\n');
+        const bles = data.match(/fe\w+0d0a/gm);
+        console.log(head)   // meta data
+        console.log(bles)   // advertisement data
+        var frame;
+        for (var i = 0; i < bles.length; i++) {
+            frame = bles[i];
+            var dataLen = parseInt(frame.substr(OFFSET_LEN, 2), 16);
+            var rssi = parseInt(frame.substr(OFFSET_RSSI, 2), 16) - 256;
+            var mac = frame.substr(OFFSET_MAC, 12);
+            // parse advertisement data
+            var advDataLen = (frame.length - OFFSET_ADV_DATA - 4) / 2;
+            var advArray = [];
+            advArray.push(advDataLen);
+            for (var j = OFFSET_ADV_DATA; j < frame.length - 4; j+= 2) {
+                advArray.push(parseInt(frame.substr(j, 2), 16));
+            }
+            var payload = new Buffer(advArray);
+            var packets = parser.parse(payload);
+            console.log(packets);
+        }
+        res.end('post')
+    });
 });
 
-//server.listen(8000);
+server.listen(8000);
 
- 
-// Payload from your BLE device (make it into a buffer, if not already)
-/*
-var payload = new Buffer([27, 2, 1, 6, 17, 6, 186, 86, 137, 166, 250, 191, 162, 189, 1, 70, 125, 110, 56, 88, 171, 173, 5, 22, 10, 24, 7, 4]);
- 
-// Parse (little-endian by default)
-var packets = parser.parse(payload);
- 
-// 
-console.log(packets.length); // 3
-console.log(packets[0].type); // Flags
-console.log(packets[0].data); //  [ 'LE Limited Discoverable Mode' ]
- 
-console.log(packets[1].type); // 'Incomplete List of 128-bit Service Class UUIDs'
-console.log(packets[1].data); // [ '0xba5689a6fabfa2bd01467d6e3858abad' ] 
-
-console.log(packets[2].type); 
-console.log(packets[2].data);
-*/
